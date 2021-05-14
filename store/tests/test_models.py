0192a8e1fb86d4e8efa0c37de05995a689e4335b
Jakub1994 <ostach2094@gmail.com>
2021-05-14 11:57:24 +0000

    home and base template, logic of products

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse
+from django.contrib.auth.models import User
 
 from store.models import Category, Product
 
@@ -15,27 +14,21 @@ class TestCategoriesModel(TestCase):
         Test Category model data insertion/types/field attributes
         """
         data = self.data1
-        self.assertTrue(isinstance(data, Category))
-        self.assertEqual(str(data), 'django')
+        self.assertTrue(isinstance(data,Category))
 
-    def test_category_url(self):
+    def test_category_model_entry(self):
         """
-        Test category model slug and URL reverse
+        Test Category model return name
         """
         data = self.data1
-        response = self.client.post(
-            reverse('store:category_list', args=[data.slug]))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(data), 'django')
 
 
 class TestProductsModel(TestCase):
     def setUp(self):
         Category.objects.create(name='django', slug='django')
         User.objects.create(username='admin')
-        self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
-                                            slug='django-beginners', price='20.00', image='django')
-        self.data2 = Product.products.create(category_id=1, title='django advanced', created_by_id=1,
-                                             slug='django-advanced', price='20.00', image='django', is_active=False)
+        self.data1 = Product.objects.create(category_id=1, title='Cactus fruit', created_by_id=1,               slug='cactus-fruit', price='13.40' image='cactus_fruit')
 
     def test_products_model_entry(self):
         """
@@ -44,21 +37,3 @@ class TestProductsModel(TestCase):
         data = self.data1
         self.assertTrue(isinstance(data, Product))
         self.assertEqual(str(data), 'django beginners')
-
-    def test_products_url(self):
-        """
-        Test product model slug and URL reverse
-        """
-        data = self.data1
-        url = reverse('store:product_detail', args=[data.slug])
-        self.assertEqual(url, '/item/django-beginners/')
-        response = self.client.post(
-            reverse('store:product_detail', args=[data.slug]))
-        self.assertEqual(response.status_code, 200)
-
-    def test_products_custom_manager_basic(self):
-        """
-        Test product model custom manager returns only active products
-        """
-        data = Product.products.all()
-        self.assertEqual(data.count(), 1)
