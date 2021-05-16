f45ab1faffce81828a647173bd89695b6570d46b
Jakub1994 <ostach2094@gmail.com>
2021-05-16 16:59:00 +0000

    chenges before the order

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'store',
     'basket',
     'account',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login/'
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
