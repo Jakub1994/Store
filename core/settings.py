93903b286c739d9e348963e59adee8f9b72c5dfa
Jakub1994 <ostach2094@gmail.com>
2021-05-17 22:47:26 +0000

    heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-o%+=s5wmg#u=n=y^%hi+ms&25+ygqmk6$2#$ivk!uulae4ho-x'
+# SECURITY WARNING: keep thes secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['store-fruity-vege.herokuapp.com', '127.0.0.1', 'localhost']
 
