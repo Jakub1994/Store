f51225344d286c4cdcbad34111d3f1155c474726
Jakub1994 <ostach2094@gmail.com>
2021-05-18 11:56:21 +0000

    refactoring

--- basket/basket.py
+++ basket/basket.py
@@ -64,7 +64,16 @@ class Basket():
         self.save()
 
     def get_total_price(self):
-        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+        
+        subtotal = sum(Decimal(item['price']) * item['qty'] for item in self.basket.values)
+
+        if subtotal == 0:
+            shipping = Decimal(0.00)
+        else:
+            shipping = Decimal(11.50)
+
+        total = subtotal + Decimal(shipping)
+        return total
 
     def delete(self, product):
         """
